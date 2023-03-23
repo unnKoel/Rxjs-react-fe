24fa5bb6b39630708d658f356de73ba6ecd66281
unnkoel <unnkoel@gmail.com>
2023-03-24 07:13:42 +0800

    add webpack configuration written by ts

---
+++ webpack.config.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+};
+
+export default config;
